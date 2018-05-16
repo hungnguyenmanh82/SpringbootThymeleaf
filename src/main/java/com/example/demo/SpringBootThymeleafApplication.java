6cf3674eba9fd9914b43f69c62164b726942b150
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2018-05-16 14:25:00 +0700

    Springboot Thymeleaf. Spring MVC

---
+++ src/main/java/com/example/demo/SpringBootThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+https://o7planning.org/en/11545/spring-boot-and-thymeleaf-tutorial
+ */
+@SpringBootApplication
+public class SpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafApplication.class, args);
+	}
+}
