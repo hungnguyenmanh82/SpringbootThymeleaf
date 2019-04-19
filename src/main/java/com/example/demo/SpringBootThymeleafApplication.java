b7ee795e7768093dc920b730c1913556573109b1
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2019-04-19 17:35:42 +0700

    update

--- src/main/java/com/example/demo/SpringBootThymeleafApplication.java
+++ src/main/java/com/example/demo/SpringBootThymeleafApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 https://o7planning.org/en/11545/spring-boot-and-thymeleaf-tutorial
  */
 @SpringBootApplication
+//để tăng performance lúc start Springboot
+//@ComponentScan({"com.in28minutes.springboot.basics.springbootin10steps","com.in28minutes.springboot.somethingelse"})
 public class SpringBootThymeleafApplication {
 
 	public static void main(String[] args) {
